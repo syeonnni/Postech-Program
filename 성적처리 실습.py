ecd49fe3d111db67a8a3a1127b3cdd1652108995
syeonnni <141931439+syeonnni@users.noreply.github.com>
2024-04-14 16:15:25 +0900

    Create 성적처리 실습.py
    성적처리 실습

---
+++ 성적처리 실습.py
@@ -0,0 +1,15 @@
+#성적처리 실습
+python101 = int(input("성적을 입력하세요: "))
+
+if python101 >= 90:
+    print("A")
+
+elif 50 <= python101 < 90:
+    print("B")
+
+else:
+    print("C")
+
+print("수고 많으셨습니다!!")
+
+
